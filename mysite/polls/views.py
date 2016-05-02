380c9fb6cda042c1b27cc7235ceda87b7c8989a8
ghostcomando <daniel-3223@hotmail.com>
2016-05-01 22:53:34 -0500

    Turorial 4
    sin terminar

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
-from .models import Question
-
-# Create your views here.
-#def index(request):
-#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#	template = loader.get_template('polls/index.html')
-#	context = {
-#		'latest_question_list': latest_question_list,
-#	}
-#	return HttpResponse(template.render(context, request))
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -25,8 +17,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-	response = "estas viendo el resultado de la pregunta %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("estas votando una pregunta %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['Choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "no has seleccionado una respuesta.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
